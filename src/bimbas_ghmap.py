6b0cc4371b7eea450b48e46599738b13e546cd9c
Cyril Moreau (cymo) <cyril.moreau07@proton.me>
2025-01-07 18:14:34 +0100

    bimbas_ghmap: Modify feature extraction to get all the features. (NR, DCA_iqr, NAR_std, NTR_IQR, NCAR_median, NCAR_gini, DCAR_gini) where removed from the extraction of RABBIT.

--- src/bimbas_ghmap.py
+++ src/bimbas_ghmap.py
@@ -2,13 +2,13 @@ import warnings
 from datetime import datetime
 from importlib.resources import files
 
-import important_features as rabbit_features
+from important_features import __stats, __convert_col_type
 import pandas as pd
 import requests.exceptions
 from ghmap.mapping.action_mapper import ActionMapper
 from ghmap.mapping.activity_mapper import ActivityMapper
 from ghmap.utils import load_json_file
-from rabbit import get_model, compute_confidence
+from rabbit import get_model, compute_confidence, check_ratelimit
 
 QUERY_ROOT = "https://api.github.com"
 
@@ -66,6 +66,8 @@ def query_user_type(contributor, api_key):
         reset_time = datetime.fromtimestamp(int(response.headers['X-RateLimit-Reset'])).strftime('%Y-%m-%d %H:%M:%S')
         print(f"Querying {contributor} : Remaining queries: {response.headers['X-RateLimit-Remaining']}. It will reset at {reset_time}")
 
+        check_ratelimit(int(response.headers['X-RateLimit-Remaining']), int(response.headers['X-RateLimit-Reset']), 4)
+
         return response.json()['type']
     else:
         print(f"Error while querying {contributor}: {response.status_code}")
@@ -82,14 +84,14 @@ def events_to_activities(events):
     """
 
     # Step 1: Event to Action Mapping
-    event_to_action_mapping_file = files("resources").joinpath("config", "event_to_action.json")
+    event_to_action_mapping_file = files("src").joinpath("resources/config", "event_to_action.json")
     action_mapping = load_json_file(event_to_action_mapping_file)
     action_mapper = ActionMapper(action_mapping)
 
     actions = action_mapper.map(events)
 
     # Step 2: Actions to Activities Mapping
-    action_to_activity_mapping_file = files("resources").joinpath("config", "action_to_activity.json")
+    action_to_activity_mapping_file = files("src").joinpath("resources/config", "action_to_activity.json")
     activity_mapping = load_json_file(action_to_activity_mapping_file)
     activity_mapper = ActivityMapper(activity_mapping)
 
@@ -126,26 +128,45 @@ def activity_to_df(act):
     return activities_df
 
 
-def extract_features(user, api_key, min_events=5, max_queries=3):
+def extract_features(df, contributor):
     """
-    args:
-    - user: The username of the user to extract features
-    - min_events: Minimum number of events to extract features. If not met, return None (default: 5)
-    - max_queries: Maximum number of queries to the GitHub API (default: 3)
-    - API_KEY: The GitHub API key
+    Extract the features from the activities of a user.
+    It uses the same features as the RABBIT feature extractor but does not remove some features
+    (NR, DCA_iqr, NAR_std, NTR_IQR, NCAR_median, NCAR_gini, DCAR_gini).
 
-    returns:
-    - A DataFrame with the features extracted from the user's activities
+    args:
+    - df: activity DataFrame with the columns 'date', 'activity', 'contributor' and 'repository (owner/repo)'
+
+    returns: A DataFrame with the features extracted from the user's activities
+
+    The features that are extracted are:
+    - NA: number of activities,
+    - NT: number of activity types,
+    - NOR: number of owners of repositories,
+    - *NR*: number of repositories,
+    - ORR: Owner repository ratio,
+    - DCA: time difference between consecutive activities (mean, median, std and gini, *IQR*),
+    - NAR: number of activities per repository (mean, median, gini and IQR, *std*),
+    - NTR: number of activity types per repository (mean, median, std and gini, *IQR),
+    - NCAR: number of continuous activities in a repo (mean, std and IQR, *median*, *gini*),
+    - DCAR: total time taken to perform consecutive activities in a repo (mean, median, std and IQR, *gini*),
+    - DAAR: time taken to switch repos (mean, median, std, gini and IQR),
+    - DCAT: time taken to switch activity type (mean, median, std, gini and IQR),
+    - NAT: number of activities per type (mean, median, std, gini and IQR).
     """
-    raw_events = query_events(user, api_key, max_queries)
-    if len(raw_events) < min_events:
-        return None
-    activities = events_to_activities(raw_events)
-    df = activity_to_df(activities)
+    df['date'] = pd.to_datetime(df.date, errors='coerce', format='%Y-%m-%dT%H:%M:%S+00:00').dt.tz_localize(None)
+    df[['owner', 'repo']] = df.repository.str.split('/', expand=True)
+
+    # Extract features using RABBIT extractor
+    df_feat = pd.json_normalize(__stats(df), sep='_')
+
     df_feat = (
-        rabbit_features.extract_features(df)
-        .set_index([[user]])  # Set the row index to the username
+        __convert_col_type(df_feat)
+        .rename(columns={'feat_NA':'NA', 'feat_NT':'NT', 'feat_NOR':'NOR', 'feat_ORR':'ORR'})
+        .sort_index()
+        .set_index([[contributor]])
     )
+
     return df_feat
 
 
@@ -161,9 +182,14 @@ def predict_contributor(user, api_key, min_events=5, max_queries=3):
     - `contributor_type`: The type of the contributor ('Bot', 'Human' or 'Unknown')
     - `confidence`: The confidence of the prediction
     """
-    df_feat = extract_features(user, api_key, min_events, max_queries)
-    if df_feat is None:
+    raw_events = query_events(user, api_key, max_queries)
+    if len(raw_events) < min_events:
         return 'Unknown', 0.0
+    activities = events_to_activities(raw_events)
+    df = activity_to_df(activities)
+
+    df_feat = extract_features(df, user)
+
     model = get_model()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
